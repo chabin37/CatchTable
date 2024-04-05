9177560416a5d31d9857ecc6e76f4fafbc8ab6bd
chabin37 <7chabin@naver.com>
2024-04-05 23:14:58 +0900

    good

--- CatchTable/src/database.java
+++ CatchTable/src/database.java
@@ -8,25 +8,22 @@ public class database {
     Scanner reserve;
     Scanner reserveManagement;
     Scanner store;
-    Scanner waiting;
     //텍스트 파일 쓰기
     PrintWriter accountWrite;
     PrintWriter reserveWrite;
     PrintWriter reserveManagementWrite;
     PrintWriter storeWrite;
-    PrintWriter waitingWrite;
-    {
+    public database(){//아마 이 생성자에서, 데이터 무결성을 검사해야 할 것으로 보임
+        //이때, 데이터 무결성 검사 클래스를 따로 빼서 구현하는것을 추천
         try {
             account = new Scanner(new File("account.txt"));
             reserve = new Scanner(new File("reserve.txt"));
             reserveManagement = new Scanner(new File("reserveManagement.txt"));
             store = new Scanner(new File("store.txt"));
-            waiting = new Scanner(new File("waiting.txt"));
-            accountWrite=new PrintWriter("account.txt");
+            accountWrite = new PrintWriter("account.txt");
             reserveWrite=new PrintWriter("reserve.txt");
             reserveManagementWrite=new PrintWriter("reserveManagement.txt");
             storeWrite=new PrintWriter("store.txt");
-            waitingWrite=new PrintWriter("waitingWrite.txt");
 
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
