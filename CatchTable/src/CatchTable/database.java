765b09372cb24c853b9012db03ce448f69f82c67
chabin37 <7chabin@naver.com>
2024-04-08 19:31:01 +0900

    good

--- CatchTable/src/CatchTable/database.java
+++ CatchTable/src/CatchTable/database.java
@@ -1,8 +1,12 @@
 package CatchTable;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 import java.io.PrintWriter;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class database {
     //텍스트 파일 읽기
     Scanner account;
@@ -14,7 +18,8 @@ public class database {
     PrintWriter storeWrite;
     PrintWriter reserveManagementWrite;
     PrintWriter reserveWrite;
-    public database(){//아마 이 생성자에서, 데이터 무결성을 검사해야 할 것으로 보임
+
+    public database() {//아마 이 생성자에서, 데이터 무결성을 검사해야 할 것으로 보임
         //이때, 데이터 무결성 검사 클래스를 따로 빼서 구현하는것을 추천
         try {
             account = new Scanner(new File("account.txt"));
@@ -23,51 +28,138 @@ public class database {
             reserve = new Scanner(new File("reserve.txt"));
 
             accountWrite = new PrintWriter("account.txt");
-            storeWrite=new PrintWriter("store.txt");
-            reserveManagementWrite=new PrintWriter("reserveManagement.txt");
-            reserveWrite=new PrintWriter("reserve.txt");
+            storeWrite = new PrintWriter("store.txt");
+            reserveManagementWrite = new PrintWriter("reserveManagement.txt");
+            reserveWrite = new PrintWriter("reserve.txt");
 
         } catch (FileNotFoundException e) {
             System.out.println("데이터베이스에 문제가 있습니다. 프로그램을 종료합니다.");
             System.exit(0);
         }
         //account, store, reserveManagement, reserve 순으로 입출력 전체 관리3435
-        isVaildElement(3,account);
-
-        isVaildElement(4,store);
-        isVaildElement(3,reserveManagement);
-        isVaildElement(5,reserve);
+        isVaildAccount(3, account);
+        isvaildStore(4, store);
+        isvaildReserveManagement(3, reserveManagement);
+        isvaildReserve(5, reserve);
     }
-    private boolean isVaildElement(int count,Scanner sc){
+    //아래의 isVaildAccount를 이용해서, 요소가 count만큼 있는지 확인, 이후 isVaild String Time Date 확인
+
+    private boolean isVaildAccount(int count, Scanner sc) {
         //구성요소의 갯수가 count만큼 있는가
-        while(sc.hasNextLine()){
+        //[ID]+[\t]+[PW]+[\t]+[사장 고객 여부]+[\n]
+        while (sc.hasNextLine()) {
             String line = sc.nextLine();
-            String[]part=line.split("\t");
-            if(part.length!=count) {
+            String[] part = line.split("\t");
+            if (part.length != count) {//갯수만큼 없는 경우
                 completionCode();
-            }
-            else{//갯수만큼 있는 경우
-                return true;
+            } else {//갯수만큼 있는 경우
+                isVaildString(0,part[0]);
+                isVaildString(0,part[1]);
+                isVaildCustomerOwner(0,part[2]);
             }
         }
         return true;
     }
-    private boolean isVaildString(int index, Scanner sc){//'단어'조건에 맞는가 (띄어쓰기가 없이 이어진 문자열)
-        while(sc.hasNextLine()){
-            String line=sc.nextLine();
+
+    private boolean isvaildStore(int count, Scanner sc) {
+        //[매장 이름]+[\t]+[ID]+[\t]+[영업 시작 시간]+[\t]+[영업 종료 시간]+[\n]
+        while (sc.hasNextLine()) {
+            String line = sc.nextLine();
+            String[] part = line.split("\t");
+            if (part.length != count) {//갯수만큼 없는 경우
+                completionCode();
+            } else {//갯수만큼 있는 경우
+                isVaildString(0,part[0]);
+                isVaildString(0,part[1]);
+            }
         }
-
-        if (.contains(" ")) {
-            completionCode();
-        }else return true;
+        return true;
     }
-    private boolean isVaildDate(String s){//날짜 문자열이 조건에 맞는가 2024/04/28
 
+    private boolean isvaildReserveManagement(int count, Scanner sc) {
+        //[매장 이름]+[\t]+[시간]+[\t]+[예약 가능 인원]+[\n]
+        while (sc.hasNextLine()) {
+            String line = sc.nextLine();
+            String[] part = line.split("\t");
+            if (part.length != count) {//갯수만큼 없는 경우
+                completionCode();
+            } else {//갯수만큼 있는 경우
+            }
+        }
+        return true;
     }
-    private boolean isVaildTime(String s){//시간 문자열이 조건에 맞는가 10:00
 
+    private boolean isvaildReserve(int count, Scanner sc) {
+        //[매장 이름]+[\t]+[ID]+[\t]+[날짜]+[\t]+[시간]+[\t]+[예약 인원]+[\n]
+        while (sc.hasNextLine()) {
+            String line = sc.nextLine();
+            String[] part = line.split("\t");
+            if (part.length != count) {//갯수만큼 없는 경우
+                completionCode();
+            } else {//갯수만큼 있는 경우
+            }
+        }
+        return true;
     }
-    private void completionCode(){//모든 읽고쓰는 객체를 닫고 프로그램 강제 종료
+
+    //int a=0인 경우 데이터 무결성 검사용, int a!=0인 경우 입력받는 데 사용.
+    public boolean isVaildString(int a, String s) {//'단어'조건에 맞는가 (띄어쓰기가 없이 이어진 문자열)
+        if (a == 0) {//무결성 검사파트
+            if (s.contains(" ")) {
+                completionCode();
+            } else return true;
+        } else {//입력이 정확한지 확인
+            if(s.contains(" ")) {
+                return false;
+            }else return true;
+        }
+        return true;
+    }
+    public boolean isVaildCustomerOwner(int a, String s){
+        if (a == 0) {//무결성 검사파트
+            if (s.equals("사장")||s.equals("고객")) {
+                return true;
+            }else completionCode();
+        }else{
+            if (s.equals("사장")||s.equals("고객")) {
+                return true;
+            }else return false;
+        }return true;
+    }
+
+    public boolean isVaildDate(int a, String s) {//날짜 문자열이 조건에 맞는가 2024/04/28
+        String regex = "^[][]:[][]:[][]$";//정규표현식
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(s);
+        if (a == 0) {//무결성 검사파트
+            if (s.contains(" ")) {
+                completionCode();
+            } else return true;
+        } else {//입력이 정확한지 확인
+            if(s.contains(" ")) {
+                return false;
+            }else return true;
+        }
+        return true;
+    }
+
+    public boolean isVaildTime(int a, String s) {//시간 문자열이 조건에 맞는가 10:00
+        String regex = "^([01]?[0-9]|2[0-3]):[0-5][0-9]$";//정규표현식
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(s);
+        if (a == 0) {//무결성 검사파트
+            if (!matcher.matches()) {
+                completionCode();
+            } else return true;
+        } else {//입력이 정확한지 확인
+            if (!matcher.matches()) {
+                return false;
+            } else return true;
+        }
+        return true;
+    }
+
+    private void completionCode() {//모든 읽고쓰는 객체를 닫고 프로그램 강제 종료
         System.out.println("데이터베이스에 문제가 있습니다. 프로그램을 종료합니다.");
         this.account.close();
         this.reserve.close();
